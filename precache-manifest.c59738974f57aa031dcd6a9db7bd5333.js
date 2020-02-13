39853cd6f1a2cbe5cfa1000f27c7f1a7f585f5bc
unknown <7Lazev@gmail.com>
2020-02-13 23:43:17 +0300

    Updates

---
+++ precache-manifest.c59738974f57aa031dcd6a9db7bd5333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b6f92e789953f10ee1505e0c0df2ee",
+    "url": "/products_list/index.html"
+  },
+  {
+    "revision": "56a83aef25826be10775",
+    "url": "/products_list/static/css/main.f885935a.chunk.css"
+  },
+  {
+    "revision": "855e7eefa75bd9376ae0",
+    "url": "/products_list/static/js/2.bc668cf0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/products_list/static/js/2.bc668cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a83aef25826be10775",
+    "url": "/products_list/static/js/main.5a2075ca.chunk.js"
+  },
+  {
+    "revision": "41da2e1f83648a9ae983",
+    "url": "/products_list/static/js/runtime-main.2a467609.js"
+  }
+]);
